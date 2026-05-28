eeaae3f0349a6f99d4717002fb767ecd6b44133f
STJ <bedivere219@gmail.com>
2026-05-28 11:05:12 +0900

    Modify for test action

--- src/main/java/com/study/app/controllers/TestController.java
+++ src/main/java/com/study/app/controllers/TestController.java
@@ -16,6 +16,6 @@ public class TestController {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("[yyyy.MM.dd] hh:mm:ss");
 		
-		return ResponseEntity.ok("Test01_" + sdf.format(System.currentTimeMillis()));
+		return ResponseEntity.ok("Test02_Actions" + sdf.format(System.currentTimeMillis()));
 	}
 }
