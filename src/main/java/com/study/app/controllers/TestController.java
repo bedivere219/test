3cf0ee140cc54427edbf14ba025200770d710938
STJ <bedivere219@gmail.com>
2026-05-28 09:53:18 +0900

    Test01

---
+++ src/main/java/com/study/app/controllers/TestController.java
@@ -0,0 +1,21 @@
+package com.study.app.controllers;
+
+import java.text.SimpleDateFormat;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+	@GetMapping
+	public ResponseEntity<String> test(){
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("[yyyy.MM.dd] hh:mm:ss");
+		
+		return ResponseEntity.ok("Test01_" + sdf.format(System.currentTimeMillis()));
+	}
+}
